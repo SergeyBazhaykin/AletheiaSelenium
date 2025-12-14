e541a8bbfc2575cc17a468700cfe0e5e3371aca8
Sergey <sergeybazhaykin@gmail.com>
2025-12-15 00:14:57 +0400

    Создание теста добавления нового виджета

---
+++ src/main/java/org/raido/pages/DashboardPage.java
@@ -0,0 +1,87 @@
+package org.raido.pages;
+
+import org.raido.utils.WaitUtils;
+
+import io.qameta.allure.Step;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+
+public class DashboardPage {
+    private final WebDriver driver;
+    private final int waitTimeInSeconds;
+
+    private final By demoDashboardLocator = By.xpath("//div[contains(@class, 'pageLayout__page-content')]//a[contains(@class, 'gridCell__grid-cell')]");
+    private final By addNewWidgetButtonLocator = By.xpath("//button[contains(@class, 'ghostButton__ghost-button')]//span[text()='Add new widget']");
+    private final By firstElementWidgetTypeStepOneLocator = By.xpath("//form[contains(@class, 'wizardFirstStepForm')]//div[text()='Launch statistics chart']");
+    private final By nextStepButtonLocator = By.xpath("//div[contains(@class, 'wizardControlsSection__buttons-block')]//span[text()='Next step']");
+    private final By addButtonLocator = By.xpath("//div[contains(@class, 'wizardControlsSection__buttons-block')]//button[text()='Add']");
+    private final By configureWidgetTypeStepTwoLocator = By.xpath("//div[contains(@class, 'filtersList__filter-list')]//span[contains(@class, 'inputRadio__toggler')]");
+    private final By widgetNameInputLocator = By.xpath("//input[@placeholder='Enter widget name']");
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String createdWidgetTitleLocatorTemplate = "//div[contains(@class, 'widgets-grid')]//div[text()='%s']";
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+        this.waitTimeInSeconds = 3;
+    }
+
+    @Step("Переход на существующий дашбоард")
+    public void clickExistingDashboard() {
+        WaitUtils.waitForElementClickable(driver, demoDashboardLocator, waitTimeInSeconds);
+        driver.findElement(demoDashboardLocator).click();
+    }
+
+    @Step("Открытие окна создания нового виджета")
+    public void openNewWidgetCreationWindow() {
+        WaitUtils.waitForElementClickable(driver, addNewWidgetButtonLocator, waitTimeInSeconds);
+        driver.findElement(addNewWidgetButtonLocator).click();
+    }
+
+    @Step("Выбор типа нового виджета на первом шаге")
+    public void pickNewWidgetTypeStepOne() {
+        WaitUtils.waitForElementClickable(driver, firstElementWidgetTypeStepOneLocator, waitTimeInSeconds);
+        driver.findElement(firstElementWidgetTypeStepOneLocator).click();
+    }
+
+    @Step("Переход к следующему шагу")
+    public void clickNextStep() {
+        WaitUtils.waitForElementClickable(driver, nextStepButtonLocator, waitTimeInSeconds);
+        driver.findElement(nextStepButtonLocator).click();
+    }
+
+    @Step("Выбор типа нового виджета на втором шаге")
+    public void pickNewWidgetTypeStepTwo() {
+        WaitUtils.waitForElementClickable(driver, configureWidgetTypeStepTwoLocator, waitTimeInSeconds);
+        driver.findElement(configureWidgetTypeStepTwoLocator).click();
+    }
+
+    @Step("Ввод названия нового виджета ({widgetName})")
+    public void inputWidgetName(String widgetName) {
+        WaitUtils.waitForElementVisibility(driver, widgetNameInputLocator, waitTimeInSeconds);
+        WebElement inputName = driver.findElement(widgetNameInputLocator);
+        inputName.clear();
+        inputName.sendKeys(widgetName);
+    }
+
+    @Step("Нажание на кнопку добавления нового виджета (после создания)")
+    public void clickAddWidget() {
+        WaitUtils.waitForElementClickable(driver, addButtonLocator, waitTimeInSeconds);
+        driver.findElement(addButtonLocator).click();
+    }
+
+    @Step("Проверка, что виджет с названием '{widgetName}' добавлен на дашбоард")
+    public boolean isWidgetAdded(String widgetName) {
+        By widgetLocator = By.xpath(String.format(createdWidgetTitleLocatorTemplate, widgetName));
+
+        try {
+            WaitUtils.waitForElementVisibility(driver, widgetLocator, waitTimeInSeconds);
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/org/raido/tests/CreatingNewWidgetTest.java
@@ -0,0 +1,50 @@
+package org.raido.tests;
+
+import org.raido.BaseTest;
+import org.raido.pages.DashboardPage;
+import org.raido.pages.LaunchesPage;
+import org.raido.pages.LoginPage;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import io.qameta.allure.Story;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Description;
+
+public class CreatingNewWidgetTest extends BaseTest {
+    @Story("Тест создания нового виджета")
+    @Owner("SergeyB")
+    @Description("Попытка авторизации и создания виджета со случайным именем")
+    @Test(description = "Тест создания нового виджета")
+    public void creatingNewWidget() {
+        SoftAssert softAssert = new SoftAssert();
+        LoginPage loginPage = new LoginPage(driver);
+        LaunchesPage launchesPage = new LaunchesPage(driver);
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+        String correctLogin = "default";
+        String correctPassword = "1q2w3e";
+        String widgetName = "testWidget" + System.currentTimeMillis();
+
+        loginPage.login(correctLogin, correctPassword);
+        loginPage.clickLogin();
+
+        launchesPage.clickDashboardPageButton();
+
+        dashboardPage.clickExistingDashboard();
+        dashboardPage.openNewWidgetCreationWindow();
+        dashboardPage.pickNewWidgetTypeStepOne();
+        dashboardPage.clickNextStep();
+        dashboardPage.pickNewWidgetTypeStepTwo();
+        dashboardPage.clickNextStep();
+        dashboardPage.inputWidgetName(widgetName);
+        dashboardPage.clickAddWidget();
+
+        softAssert.assertTrue(dashboardPage.isWidgetAdded(widgetName),
+                "После попытки создания виджета " + widgetName + ", данный виджет не найден на странице дашборда");
+
+        softAssert.assertAll();
+
+    }
+}
